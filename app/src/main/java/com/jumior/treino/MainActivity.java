10a430fc36b57b693d1c2bec60da81d129cd652f
junior929201 <125149668+junior929201@users.noreply.github.com>
2023-08-11 18:03:24 +0000

    hgfdhgbjfd

--- app/src/main/java/com/jumior/treino/MainActivity.java
+++ app/src/main/java/com/jumior/treino/MainActivity.java
@@ -1,25 +1,54 @@
 package com.jumior.treino;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.*;
 import android.util.Log;
 import android.content.Context;
 import android.widget.Toast;
+
 import com.jumior.treino.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+
 import com.itsaky.androidide.logsender.LogSender;
 
 public class MainActivity extends AppCompatActivity {
 
-	private ActivityMainBinding binding;
-	
+    private Button btnCriar, btnSelecionar, btnExcluir;
+    private EditText editNome;
+    private ArrayList<String> taskList;
+    private ArrayAdapter<String> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-		// Remove this line if you don't want AndroidIDE to show this app's logs
-		LogSender.startLogging(this);
+
         super.onCreate(savedInstanceState);
-        // Inflate and get instance of binding
-		binding = ActivityMainBinding.inflate(getLayoutInflater());
-        // set content view to binding's root
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main);
+
+        btnCriar = findViewById(R.id.btnCriar);
+        btnExcluir = findViewById(R.id.btnExcluir);
+        btnSelecionar = findViewById(R.id.btnSelecionar);
+        editNome = findViewById(R.id.editNome);
+
+
+        btnCriar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createTaskSwitch();
+            }
+        });
+
+
+        taskList = new ArrayList<>();
+        taskListView = findViewById(R.id.taskList);
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+
+        taskListView.setAdapter(adapter);
     }
+
+    private void createTaskSwitch() {
+        EditText
+    }
+
+
 }
